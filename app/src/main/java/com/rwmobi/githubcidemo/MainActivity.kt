d9628b836b06143c876a607fa8f3e12de75aba52
Ryan W <ryan@rwmobi.com>
2025-08-17 17:29:55 +0100

    [chore] App: minor edge-to-edge and UI template changes

--- app/src/main/java/com/rwmobi/githubcidemo/MainActivity.kt
+++ app/src/main/java/com/rwmobi/githubcidemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.rwmobi.githubcidemo
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import com.rwmobi.githubcidemo.ui.theme.GithubCIDemoTheme
 
@@ -20,23 +22,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // allows your app to be edge-to-edge on Android 15 before.
-        enableEdgeToEdge()
-
         setContent {
             GithubCIDemoTheme {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.dark(
+                        scrim = Color.Transparent.toArgb(),
+                    ),
+                    navigationBarStyle = SystemBarStyle.dark(
+                        scrim = Color.Transparent.toArgb(),
+                    ),
+                )
+
                 // Android 15+ Will see solid red status bar
                 // and red tinted navigation bar
                 // Pre Android 15 will see tinted navigation bar
                 // BUT status bar is not affected
                 Surface(
-                    modifier =
-                    Modifier
+                    modifier = Modifier
                         .background(color = Color.Red) // for system bar area
                         .fillMaxSize()
-                        .safeDrawingPadding()
-                        .background(color = MaterialTheme.colorScheme.background),
-                    // for actual content background
+                        .safeDrawingPadding(),
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     Greeting("Android")
                 }
